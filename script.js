f3008742edf41bbe2f53f8cf9dcf457f38a337a0
ch4ne5teban <ca1505pr@outlook.com>
2023-05-18 06:31:28 -0600

    feat: js: use for loop to create clickable div for each day of the month

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 
-
-
+//create calendar grid day divs
+const days = 31;
+const calendar = document.querySelector(".calendar");
+for (let i = 0; i < days; i++) {
+  const day = document.createElement("div");
+  day.textContent = i + 1;
+  day.addEventListener("click", () => openModal(i));
+  calendar.appendChild(day);
+}
 
 
 //Prevent negative integer input
